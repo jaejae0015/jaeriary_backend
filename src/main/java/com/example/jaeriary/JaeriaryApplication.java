7e82b71e18fb2dde1aac24b421e25e89a55f3aaa
jaejae0015 <salangae00@naver.com>
2025-12-19 17:49:17 +0900

    start Project!!!!!

---
+++ src/main/java/com/example/jaeriary/JaeriaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaeriary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaeriaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaeriaryApplication.class, args);
+	}
+
+}
